0f613f5ed9f554c4415894355399101b4b72f85a
M <martin@mcneeladesign.com>
2018-04-01 22:34:30 +0100

    * change contact name.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -12,7 +12,7 @@ class Contact extends React.Component {
       <div className="Contact">
         <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
         <BaseLayout title="Contact">
-          <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form name="contact-petra" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="contact-form" value="contact" />
             <input placeholder="Your name here..." type="text" name="name" />
             <input placeholder="Your email here..." type="email" name="email" />
