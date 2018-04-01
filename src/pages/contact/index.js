d49b97bd7c0726b950bdfddbaf752b486153e2b5
M <martin@mcneeladesign.com>
2018-04-01 21:48:27 +0100

    * react form - master

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,29 +6,80 @@ import Helmet from 'react-helmet'
 import BaseLayout from '../../components/BaseLayout'
 import Menu from '../../components/Menu'
 
-class Contact extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
-    return (
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "react-form": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    // return ( //Try ajax form on master - form submission only works on production.
+    //   <div className="Contact">
+    //     <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
+    //     <BaseLayout title="Contact">
+    //       <p>Form will be here</p>
+    //       <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    //         <input type="hidden" name="contact-form" value="contact" />
+    //         <label>Name:
+    //           <input type="text" name="name" />
+    //         </label>
+    //         <label>Email:
+    //           <input type="email" name="name" />
+    //         </label>
+    //         <label>Message:
+    //           <textarea name="message"></textarea>
+    //         </label>
+    //         <div netlify-recaptcha></div>
+    //         <button type="submit">Send</button>
+    //       </form>
+    //     </BaseLayout>
+    //   </div>
+    // )
+    return ( //Trying ajax form on master - form submission only works on production.
       <div className="Contact">
         <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
         <BaseLayout title="Contact">
           <p>Form will be here</p>
-          <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input type="hidden" name="contact-form" value="contact" />
-            <label>Name:
-              <input type="text" name="name" />
-            </label>
-            <label>Email:
-              <input type="email" name="name" />
-            </label>
-            <label>Message:
-              <textarea name="message"></textarea>
-            </label>
-            <div netlify-recaptcha></div>
-            <button type="submit">Send</button>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
           </form>
         </BaseLayout>
       </div>
