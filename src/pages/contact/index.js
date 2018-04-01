487d889d141d7e9e428ee39825b83981f2f5b2f6
M <martin@mcneeladesign.com>
2018-04-01 15:56:11 +0100

    * updated form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -7,22 +7,22 @@ import BaseLayout from '../../components/BaseLayout'
 import Menu from '../../components/Menu'
 
 class Contact extends React.Component {
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  handleSubmit = e => {
-    console.log('...this.state', ...this.state);
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
+  // handleChange = (e) => {
+  //   this.setState({[e.target.name]: e.target.value});
+  // }
+  //
+  // handleSubmit = e => {
+  //   console.log('...this.state', ...this.state);
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact", ...this.state })
+  //   })
+  //     .then(() => alert("Success!"))
+  //     .catch(error => alert(error));
+  //
+  //   e.preventDefault();
+  // };
 
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
@@ -33,16 +33,23 @@ class Contact extends React.Component {
         <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
         <BaseLayout title="Contact">
           <p>Form will be here</p>
-          <form name="contact" netlify-honeypot="bot-field" method="POST" action="thank-you" netlify onSubmit={this.handleSubmit}>
+          <form
+            name="contact"
+            netlify-honeypot="bot-field"
+            method="post"
+            // action="thank-you"
+            data-netlify="true"
+            // onSubmit={this.handleSubmit}
+          >
             <label style={{display: 'none'}}>honeypot<input name="bot-field" /></label>
             <label>Name:
-              <input type="text" name="name" onChange={this.handleChange} />
+              <input type="text" name="name" />
             </label>
             <label>Email:
-              <input type="email" name="email" onChange={this.handleChange} />
+              <input type="email" name="email" />
             </label>
             <label>Message:
-              <textarea name="message" onChange={this.handleChange}></textarea>
+              <textarea name="message"></textarea>
             </label>
             <button type="submit">Send</button>
           </form>
