95be1dd544cbf975443e7b117d65b173389ba08c
M <martin@mcneeladesign.com>
2018-04-01 22:23:04 +0100

    * go something something.

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -45,7 +45,7 @@ class Contact extends React.Component {
               <input type="text" name="name" />
             </label>
             <label>Email:
-              <input type="email" name="name" />
+              <input type="email" name="email" />
             </label>
             <label>Message:
               <textarea name="message"></textarea>
